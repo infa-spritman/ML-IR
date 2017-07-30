8cf47c717bc604aecabce86c381e44d9d827e0cc
Sushant Pritmani <whisprrind.twister@gmail.com>
2017-07-29 20:58:48 -0400

    First Commit : read qrel and write proc

---
+++ Matrix_Generation.py
@@ -0,0 +1,126 @@
+from elasticsearch import Elasticsearch
+import sys
+from collections import defaultdict, OrderedDict
+import operator
+import math
+
+es = Elasticsearch()
+
+
+def read_qrel(file_qrel):
+    temp_qrel = defaultdict(lambda: defaultdict(lambda: 0))
+    temp_num_rel = defaultdict(lambda: 0)
+
+    f = open(file_qrel, 'r')
+    l = f.readlines()
+    for line in l:
+        try:
+            topic, dummy, doc_id, rel = line.split()
+            rel = int(rel)
+            temp_qrel[int(topic)][doc_id] = rel
+            if rel > 0:
+                temp_num_rel[int(topic)] += 1
+            else:
+                temp_num_rel[int(topic)] += 0
+        except Exception, e:
+            print e
+    f.close()
+    return temp_qrel, temp_num_rel
+
+
+# Citation : https://gist.github.com/drorata/146ce50807d16fd4a6aa (  for scroll )
+
+def read_all_docs(indexName, type):
+    doc_set = set()
+    page = es.search(
+        index=indexName,
+        doc_type=type,
+        scroll='2m',
+        _source_include=['docno'],
+        size=1000,
+        body={
+            "query": {
+                "match_all": {}
+            }
+        })
+    sid = page['_scroll_id']
+    scroll_size = page['hits']['total']
+
+    print scroll_size
+    # Start scrolling
+    while (scroll_size > 0):
+
+        for hit in page['hits']['hits']:
+            doc_set.add(str(hit['_source']['docno']))
+
+
+        print "Scrolling..."
+        page = es.scroll(scroll_id=sid, scroll='2m')
+        # Update the scroll ID
+        sid = page['_scroll_id']
+        # Get the number of results that we returned in the last scroll
+        scroll_size = len(page['hits']['hits'])
+        # print "scroll size: " + str(scroll_size)
+
+
+
+    return doc_set
+
+
+def getScoreTuple(docid):
+    return (1,2,3,4,5)
+
+
+def write_matrix(query, temp_query_map, test_queries_set):
+    if query in test_queries_set:
+        with open('test_matrix.txt', 'a+') as f:
+            for docId, score_tuple in temp_query_map.iteritems():
+                temp_str = []
+                temp_str.append(str(query) + '-' + str(docid))
+                for index,eval_system_score in enumerate(score_tuple):
+                    temp_str.append(str(eval_system_score))
+
+                string_write = ' '.join(temp_str) + '\n'
+
+                f.write(string_write)
+    else:
+        with open('train_matrix.txt', 'a+') as f:
+            for docId, score_tuple in temp_query_map.iteritems():
+                temp_str = []
+                temp_str.append(str(query) + '-' + str(docid))
+                for index,eval_system_score in enumerate(score_tuple):
+                    temp_str.append(str(eval_system_score))
+
+                string_write = ' '.join(temp_str) + '\n'
+
+                f.write(string_write)
+
+
+
+
+
+if __name__ == '__main__':
+    test_queries_set = set([56, 57, 64, 71, 99])
+    # queries_map = read_queries()
+    all_docs = read_all_docs('ap_dataset', 'hw1')
+    qrel, num_rel = read_qrel('qrels.adhoc.51-100.AP89.txt')
+    for query, docMap in qrel.iteritems():
+        count = 0
+        temp_query_map = defaultdict(lambda: ())
+        for docid, relvance in docMap.iteritems():
+            all_score_tuple = getScoreTuple(docid)
+            temp_query_map[docid] = all_score_tuple
+            count += 1
+
+        if count <1000:
+            for docid in all_docs:
+                if count==1000:
+                    write_matrix(query,temp_query_map,test_queries_set)
+                    break
+
+                if docid not in temp_query_map:
+                    all_score_tuple = getScoreTuple(docid)
+                    temp_query_map[docid] = all_score_tuple
+                    count += 1
+
+
